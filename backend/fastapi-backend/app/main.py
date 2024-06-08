810cd49bcbbdeba9c05db186d3367af2b1e382ae
J-Raymer <44129402+J-Raymer@users.noreply.github.com>
2024-06-07 22:22:38 -0700

    set up reverse proxy to fb-backend

--- backend/fastapi-backend/app/main.py
+++ backend/fastapi-backend/app/main.py
@@ -5,6 +5,13 @@ from fastapi import FastAPI, Path, Query
 app = FastAPI()
 
 
-@app.get("/")
-def index():
+@app.get("/api/{path:path}")
+async def index():
     return {"hello": "UVic Marketplace FB"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Run the server using UVicorn with specified host and port
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
--- backend/reverse-proxy/app/main.py
+++ backend/reverse-proxy/app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+import httpx
+
 
 app = FastAPI()
 
+# Define the URL of your backend FastAPI server
+backend_url = "http://localhost:8001"
 
-@app.get("/")
-def index():
-    return {"hello": "UVic Marketplace Proxy"}
+# Proxy API requests to the backend for specific paths
+# TODO: should we handle path validation here or in fb-backend?
+@app.get("/api/{path:path}")
+async def proxy_api_request(path: str):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(backend_url + f"/api/{path}")
+            return response.text
+        except httpx.HTTPError as exc:
+            raise HTTPException(status_code=exc.response.status_code, detail=exc.response.text)
+
+
+# Catch-all route to handle requests to other paths
+@app.get("/{path:path}")
+async def proxy_other_request(path: str):
+    raise HTTPException(status_code=404, detail="Path not found")
