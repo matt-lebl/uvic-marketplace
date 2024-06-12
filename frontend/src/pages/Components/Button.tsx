59b11412767d937c6280452ab90f05d03700b98a
KyleSullivanGitHub <75002173+KyleSullivanGitHub@users.noreply.github.com>
2024-06-11 17:14:17 -0700

    Update Button.tsx
    Removed CSS setup. We'll be using themes instead

--- frontend/src/pages/Components/Button.tsx
+++ frontend/src/pages/Components/Button.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactButton from '@mui/material/Button';
-import '../App.css';
 
 interface Props {
     id: string;
     label?: string;
-    className?: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-const Button: React.FC<Props> = ({ id, label, className, onClick }) => {
+const Button: React.FC<Props> = ({ id, label, onClick }) => {
     return <ReactButton 
         id={id}
         data-testid={id}
-        className={className}
         onClick={onClick}
     >{label}</ReactButton>;
 };
