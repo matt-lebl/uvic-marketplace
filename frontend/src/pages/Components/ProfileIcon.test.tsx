d59f519c02e5c8007d5527ce11d0588b380720aa
KyleSullivanGitHub <75002173+KyleSullivanGitHub@users.noreply.github.com>
2024-06-13 00:37:51 +0000

    Completed Implementation of Profile Icon

--- frontend/src/pages/Components/ProfileIcon.test.tsx
+++ frontend/src/pages/Components/ProfileIcon.test.tsx
@@ -2,37 +2,29 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react'
 import ProfileIcon from './ProfileIcon';
 
-describe('Button', () => {    
+describe('ProfileIcon', () => {    
     
-    test('renders Icon with correct id property and default settings (Initials for an icon)', () => {
-        const id: string = 'button-id';
+    test('renders Icon with correct id property and default settings (First Initial for an icon)', () => {
+        const id: string = 'icon-id';
         const name: string = 'Test Name';
         render(<ProfileIcon id={id} name={name} />);
         const icon = screen.getByTestId(id);
+        console.log(icon.outerHTML);
+
         expect(icon).toHaveAttribute('id', id);
         expect(icon.querySelector('img')).toBeNull();
-        expect(icon.textContent).toBe('TN');
+        expect(icon.textContent).toBe("TN");
     });
 
     test('renders Icon with correct image', () => {
-        const id: string = 'button-id';
+        const id: string = 'icon-id';
         const name: string = 'Test Name';
-        const imageSrc: string = 'frontend/public/favicon.ico';
+        const imageSrc: string = './Test_Resources/TestProfileImage.jpg';
         render(<ProfileIcon id={id} name={name} imageSrc={imageSrc} />);
         const icon = screen.getByTestId(id);
-        const avatarImg = icon.querySelector('img');
-        expect(avatarImg).not.toBeNull();
-        expect(avatarImg).toHaveAttribute('src', imageSrc);
-    });
-
-    test('renders Icon with no name if set to be icon only', () => {
-        const id: string = 'button-id';
-        const name: string = 'Test Name';
-        render(<ProfileIcon id={id} name={name} />);
-        const icon = screen.getByTestId(id);
-        expect(icon).toHaveAttribute('id', id);
-        expect(icon.querySelector('img')).toBeNull();
-        expect(icon.textContent).toBe('TN');
+        const avatar = icon.querySelector('img');
+        expect(avatar).not.toBeNull();
+        expect(avatar).toHaveAttribute('src', imageSrc);
     });
 
     test('renders Icon with working nav link to Profile Page of person.', () => {
