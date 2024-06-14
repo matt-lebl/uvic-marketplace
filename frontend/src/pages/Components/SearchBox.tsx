5a8c092cb9afe31634b2dbb1d55c62d7ad45d862
Ethan McCullagh <84647308+ethan89762@users.noreply.github.com>
2024-06-13 18:32:54 -0700

    basic functionality working

---
+++ frontend/src/pages/Components/SearchBox.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Paper, IconButton, InputBase } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { SxProps } from '@mui/material';
+import { useState } from 'react';
+import { ChangeEvent, FormEvent } from 'react';
+
+
+interface Props {
+    id: string
+    placeholder?: string | undefined
+    sx?: SxProps | undefined
+    submit: (out:string) => void
+}
+
+const Searchbox: React.FC<Props> = ({ placeholder, sx, id, submit }) => {
+
+    const [input, setInput] = useState('')
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInput(event.target.value)
+    }
+
+    const handleSubmit = (event:FormEvent) => {
+        event.preventDefault()
+        submit(input)
+    }
+
+    const handleClick = () => {
+        submit(input)
+    }
+    return (
+        <Paper
+            component="form"
+            sx={sx}
+            id={id}
+            onSubmit={handleSubmit}
+        >
+            <InputBase
+                id='Search Field'
+                placeholder={placeholder}
+                sx={{ ml: 1, flex: 1 }}
+                onChange={handleChange} 
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleClick}>
+                <SearchIcon />
+            </IconButton>
+        </Paper>
+
+    )
+
+}
+
+export default Searchbox;
