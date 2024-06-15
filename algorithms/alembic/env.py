96b5c0217623ac1d00227ae9e1d7913682a07b59
Ross Goldsand <ross.goldsand@gmail.com>
2024-06-15 10:30:05 -0700

    Add environment variable to alembic.ini

--- algorithms/alembic/env.py
+++ algorithms/alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -15,6 +16,8 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
