26f2fbb0aa5f077093859068a075ccd52576aa50
ebertc <conwebert@gmail.com>
2024-06-18 17:53:44 -0700

    WIP updating file structure

---
+++ backend/data-layer/app/routers/users.py
@@ -0,0 +1,17 @@
+@app.post("/users/", response_model=User)
+def create_user(user: UserBase, session: Session = Depends(get_session)):
+    user_data = user.dict()
+    user_data["user_id"] = str(uuid.uuid4())
+    new_user = User.create(session=session, **user_data)
+    return new_user
+
+@app.get("/users/", response_model=List[User])
+def get_all_users(session: Session = Depends(get_session)):
+    return User.get_all(session)
+
+@app.get("/users/{user_id}", response_model=User)
+def get_user(user_id: str, session: Session = Depends(get_session)):
+    user = User.get_by_id(session, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
