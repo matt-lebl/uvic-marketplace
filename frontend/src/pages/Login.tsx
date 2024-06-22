650fdf438a642d7c05ebde9afac580604a435dc4
Stephen Neale <stephenneale@uvic.ca>
2024-06-21 17:39:29 -0700

    Refactor login-form to a component

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,6 @@
-import { ErrorSharp } from '@mui/icons-material'
 import './App.css'
-import { Typography, FormControl, TextField, Box, FormHelperText } from '@mui/material'
-import { Button } from '@mui/material'
-import { error } from 'console'
-import { Formik } from 'formik'
+import { Typography, Box } from '@mui/material'
+import LoginForm from './Components/LoginForm'
 
 function Login() {
   return (
@@ -15,74 +12,7 @@ function Login() {
             Login
           </Typography>
 
-          <Formik
-            initialValues={{ email: '', password: '' }}
-            validate={values => {
-              const errors: Record<string, string> = {}
-              if (!values.email) {
-                errors.email = 'Required'
-              } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-              ) {
-                errors.email = 'Invalid email address'
-              }
-              if (!values.password) {
-                errors.password = 'Required'
-              }
-              return errors
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                // TODO: Implement login hook and redirect to home page on success
-                alert(JSON.stringify(values, null, 2))
-                setSubmitting(false)
-              }, 400)
-            }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-            }) => (
-              <form onSubmit={handleSubmit} data-testid="login-form">
-                <FormControl fullWidth>
-                  <TextField
-                    type="email"
-                    name="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    label="Email"
-                    margin="normal"
-                    error={Boolean(errors.email) && touched.email}
-                  />
-                  <FormHelperText style={{ color: 'red' }}>{errors.email}</FormHelperText>
-                </FormControl>
-                <FormControl fullWidth>
-                  <TextField
-                    type="password"
-                    name="password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    label="Password"
-                    margin="normal"
-                    error={Boolean(errors.password) && touched.password}
-                  />
-                  <FormHelperText style={{ color: 'red' }}>{errors.password}</FormHelperText>
-                </FormControl>
-                <div>
-                  <Button type="submit" disabled={isSubmitting} variant="contained" color="primary" sx={{ my:2 }}>
-                    Submit
-                  </Button>
-                </div>
-              </form>
-            )}
-          </Formik>
+          <LoginForm />
         </Box>
 
       </header>
