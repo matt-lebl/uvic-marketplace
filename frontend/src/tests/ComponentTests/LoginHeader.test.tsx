114e3d8581bfc620d4faa4a97d71af756f68786f
Stephen Neale <stephenneale@uvic.ca>
2024-06-21 17:03:22 -0700

    Update header to reflect mocks

--- frontend/src/tests/ComponentTests/LoginHeader.test.tsx
+++ frontend/src/tests/ComponentTests/LoginHeader.test.tsx
@@ -7,6 +7,6 @@ import LoginHeader from '../../pages/Components/LoginHeader'
 
 test('renders listing page', () => {
   render(<LoginHeader />)
-  const linkElement = screen.getByText(/Login Header/i)
+  const linkElement = screen.getByText(/UVic Marketplace/i)
   expect(linkElement).toBeInTheDocument()
 })
