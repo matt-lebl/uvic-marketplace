4f1c0e0c1acc2a52aeccfd961c6d44650682e30b
Adam-Tidball <abtidball@gmail.com>
2024-06-22 23:37:22 -0700

    added plan for getting userID

--- algorithms/app/api/endpoints/recommendations.py
+++ algorithms/app/api/endpoints/recommendations.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Query, Header, HTTPException
 from typing import List
 from ...schemas import ListingSummary, ErrorMessage
+# import base64
+# import json
 
 router = APIRouter()
 
@@ -8,11 +10,29 @@ router = APIRouter()
 async def recommendations(page: int = Query(1),
                           limit: int = Query(20),
                           authorization: str = Header(...)):
+   
     # SQL query logic here to fetch recommendations, I think sqlalchemy can be used to query the database
-   # return []
+        # Requirements
+        # - provides suggestion results for 'item' 
+        #      - core (15) based on users' click/view on item listing
+        #      - additional (15) based on user reviews, search reuslts, purchase history
 
     # TODO: get userID of who is looking for recommendations
-    # TODO: generate recommendations based on userID
+    # try:
+    #     # Assuming the token is a Bearer token
+    #     token = authorization.split(" ")[1]  # Split the header to get the token part
+    #     payload_base64 = token.split('.')[1]  # Get the payload part of the token
+    #     payload_bytes = base64.urlsafe_b64decode(payload_base64 + '==')  # Decode Base64
+    #     payload = json.loads(payload_bytes)  # Convert JSON string to dictionary
+    #     user_id = payload['user_id']  # Extract user_id from the token payload
+    # except Exception as e:
+    #     raise HTTPException(status_code=401, detail="Invalid token")
+
+    # temp userID - without real token auth
+    user_id = "user123"
+
+
+    # TODO: get recommendations based on userID
     # TODO: return recommendations
 
     #TEST
@@ -22,7 +42,7 @@ async def recommendations(page: int = Query(1),
         {
             "listingID": "testing",
             "sellerID": "seller456",
-            "sellerName": "John Doe",
+            "sellerName": "Guy Random",
             "title": "Used Calculus Textbook",
             "description": "No wear and tear, drop-off available.",
             "price": 50,
