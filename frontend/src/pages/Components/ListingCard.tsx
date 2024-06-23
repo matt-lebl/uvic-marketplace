efa81c8e101fe1c5cbaa5c888ca9080857916b8c
Stephen Neale <stephenneale@uvic.ca>
2024-06-22 20:38:34 -0700

    Add listing cards

--- frontend/src/pages/Components/ListingCard.tsx
+++ frontend/src/pages/Components/ListingCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
-export default function ListingCard() {
+export default function ListingCard(listing: any) {
   return (
     <Box sx={{
       display: 'flex',
@@ -9,21 +9,29 @@ export default function ListingCard() {
       flexWrap: 'nowrap',
       alignItems: 'center',
       marginTop: 3,
-      width: '90%'
+      width: '100%',
     }}>
       <Box sx={{
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         flexWrap: 'nowrap',
         alignItems: 'center',
+        justifyContent: 'space-between',
         border: '1px solid black',
+        background: 'white',
         borderRadius: '10px',
+        boxShadow: 2,
         width: '100%',
         padding: 2
       }}>
-        <Typography variant="h6">Listing Picture</Typography>
-        <Typography variant="body1">Listing Title</Typography>
-        <Typography variant="body2">Listing Price</Typography>
+        <Box>
+            <Typography variant="h6" alignSelf={"flex-start"}>{listing.title}</Typography>
+            <Typography variant="body1" alignSelf={"flex-start"}>{listing.description}</Typography>
+            <Typography variant="h6" alignSelf={"flex-start"}>{listing.price}</Typography>
+        </Box>
+        <Box>
+            <img src="https://via.placeholder.com/150" alt="placeholder" />
+        </Box>
       </Box>
     </Box>
   )
