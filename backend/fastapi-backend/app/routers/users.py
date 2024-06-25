b322beab11807de3e0b2340a8e69a1e307285f1d
rahulniraula2 <rahulniraula47@gmail.com>
2024-06-25 13:05:15 -0700

    added signing in with email and password

--- backend/fastapi-backend/app/routers/users.py
+++ backend/fastapi-backend/app/routers/users.py
@@ -1,66 +1,75 @@
-from fastapi import APIRouter
-from schemas import LoginRequest, NewUser, EmailModel, UpdateUser
+from fastapi import APIRouter, HTTPException
+from schemas import LoginRequest, NewUser, EmailModel, UpdateUser, User
 from services.data_layer_connect import send_request_to_data_layer
 
 userRouter = APIRouter(
     prefix="/api/user",
     tags=["users"],
-    responses={404: {"description": "Not found"}},
+    responses={404: {"description": "Not found"}, 401: {"description": "Unauthorized"}},
 )
 
 
 ## Auth Not Required
 @userRouter.post("/")
-def create_user(user: NewUser):
+async def create_user(user: NewUser):
 
     path = "users/"
-    return send_request_to_data_layer(path, "POST", user.model_dump())
+    response = await send_request_to_data_layer(path, "POST", user.model_dump())
+    return response.json()
 
 
 @userRouter.get("/{id}")
-def get_user(id: str, authUserID: str):
+async def get_user(id: str, authUserID: str):
 
     path = "users/" + id
-    return send_request_to_data_layer(path, "GET")
+    response = await send_request_to_data_layer(path, "GET")
+    return response.json()
 
 
 @userRouter.patch("/")
-def edit_user(user: UpdateUser, authUserID: str):
+async def edit_user(user: UpdateUser, authUserID: str):
 
     path = "users/" + authUserID
-    return send_request_to_data_layer(path, "PATCH", user.model_dump())
+    response = await send_request_to_data_layer(path, "PATCH", user.model_dump())
+    return response.json()
 
 
 @userRouter.delete("/")
-def delete_user(authUserID: str):
+async def delete_user(authUserID: str):
 
     path = "users/" + authUserID
-    return send_request_to_data_layer(path, "DELETE")
+    response = await send_request_to_data_layer(path, "DELETE")
+    return response.json()
 
 
 ## Auth Not Required
 @userRouter.post("/reset-password")
-def reset_password(emailModel: EmailModel):
+async def reset_password(emailModel: EmailModel):
     # TODO: Implement password reset
     return {"TODO": "Password reset email sent to {}".format(emailModel.email)}
 
 
 ## Auth Not Required
 @userRouter.post("/login")
-def login(loginRequest: LoginRequest):
-
-    path = "users/login"
-    return send_request_to_data_layer(path, "POST", loginRequest.model_dump())
+async def login(loginRequest: LoginRequest):
+    path = "user/login"
+    email_password = {"email": loginRequest.email, "password": loginRequest.password}
+    try:
+        loginResponse = await send_request_to_data_layer(path, "POST", email_password)
+        if loginResponse.status_code == 200:
+            return loginResponse.json()
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
 @userRouter.post("/logout")
-def logout(authUserID: str):
+async def logout(authUserID: str):
     # TODO: Implement user logout
     return {"TODO": "User logged out", "Reqested by": authUserID}
 
 
 @userRouter.post("/send-confirmation-email")
-def send_confirmation_email(emailModel: EmailModel, authUserID: str):
+async def send_confirmation_email(emailModel: EmailModel, authUserID: str):
     # TODO: Implement sending confirmation email
     return {
         "TODO": "Confirmation email sent to {}".format(emailModel.email),
@@ -69,6 +78,6 @@ def send_confirmation_email(emailModel: EmailModel, authUserID: str):
 
 
 @userRouter.post("/confirm-email")
-def confirm_email(token: str, authUserID: str):
+async def confirm_email(token: str, authUserID: str):
     # TODO: Implement email confirmation
     return {"TODO": "Email confirmed", "Reqested by": authUserID}
