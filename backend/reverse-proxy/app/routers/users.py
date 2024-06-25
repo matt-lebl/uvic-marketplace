288f7644e8e54aea586fc260a576da4adabd9a70
rahulniraula2 <rahulniraula47@gmail.com>
2024-06-25 12:38:34 -0700

    added set-cookie for login on reverse proxy

--- backend/reverse-proxy/app/routers/users.py
+++ backend/reverse-proxy/app/routers/users.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
-from schemas import NewUser, LoginRequest, ResetPassword
+from fastapi import APIRouter, Response
+from schemas import NewUser, LoginRequest, ResetPassword, User
+from auth import sign_jwt
 from services.backend_connect import send_request_to_backend
 
 
@@ -10,19 +11,37 @@ usersRouter = APIRouter(
 
 ## These endpoints can be interacted with without a valid JWT token
 @usersRouter.post("/")
-async def create_user(user: NewUser):
-    return await send_request_to_backend("user", "POST", user.model_dump())
+async def create_user(
+    user: NewUser,
+):
+    response_backend = await send_request_to_backend(
+        "user", "POST", user.model_dump()
+    ).json()
+    return response_backend
 
 
 @usersRouter.post("/reset-password")
 async def reset_password(resetPassword: ResetPassword):
-    return await send_request_to_backend(
+    response_backend = await send_request_to_backend(
         "user/reset-password", "POST", resetPassword.model_dump()
-    )
+    ).json()
+    return response_backend
 
 
 @usersRouter.post("/login")
-async def login(loginRequest: LoginRequest):
-    return await send_request_to_backend(
+async def login(loginRequest: LoginRequest, response: Response):
+    response_backend = await send_request_to_backend(
         "user/login", "POST", loginRequest.model_dump()
     )
+
+    user: User = response_backend.json()
+
+    if response_backend.status_code == 200:
+        response.set_cookie(
+            key="jwt",
+            value=sign_jwt(user["userID"]),
+            httponly=True,
+            samesite="strict",
+        )
+
+    return user
