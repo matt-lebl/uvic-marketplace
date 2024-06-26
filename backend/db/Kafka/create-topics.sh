c772d48dc2ca5419f7df5acb8d09068058991c3c
rahulniraula2 <rahulniraula47@gmail.com>
2024-06-25 18:12:52 -0700

    updated docker-compose

--- backend/db/Kafka/create-topics.sh
+++ backend/db/Kafka/create-topics.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "Creating Kafka topics..."
+echo "$KAFKA_NODE_HOSTNAME"
+echo "$KAFKA_NODE_PORT"
+
 # Wait for Kafka to be ready
-while ! nc -z kafka-node-1 19092; do
-  echo "Waiting for kafka-node-1" && sleep 20
+while ! nc -z "$KAFKA_NODE_HOSTNAME" "$KAFKA_NODE_PORT"; do
+  echo "Waiting for Kafka Node to come online" && sleep 1
 done
 
 # List of topics to create
 topics="create-listing delete-listing update-listing view-listing create-user edit-user delete-user"
 
 for topic in $topics; do
-  ./opt/kafka/bin/kafka-topics.sh --create --topic "$topic" --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server kafka-node-1:19092
+  ./opt/kafka/bin/kafka-topics.sh --create --topic "$topic" --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server "$KAFKA_NODE_HOSTNAME":"$KAFKA_NODE_PORT"
 done
