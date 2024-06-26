623f61f9edffe7a71e611f5c884726cf03351915
KyleSullivanGitHub <75002173+KyleSullivanGitHub@users.noreply.github.com>
2024-06-26 15:26:30 -0700

    Update ListingCard.tsx

--- frontend/src/pages/Components/ListingCard.tsx
+++ frontend/src/pages/Components/ListingCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Button } from '@mui/material'
 
 export default function ListingCard(listing: any) {
   return (
