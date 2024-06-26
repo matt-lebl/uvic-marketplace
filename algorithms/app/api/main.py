89de3c70ed13fc2ef99f757e84e9470f1273a6c2
Ross Goldsand <ross.goldsand@gmail.com>
2024-06-25 18:56:10 -0700

    Put HTTP functions back in algorithms

--- algorithms/app/api/main.py
+++ algorithms/app/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from .endpoints import search, recommendations
+from app.api.endpoints import search, recommendations, listings, interactions
 
 app = FastAPI()
 
+app.include_router(listings.router, prefix="/api", tags=["Listings"])
 app.include_router(search.router, prefix="/api", tags=["Search"])
 app.include_router(recommendations.router, prefix="/api", tags=["Recommendations"])
+app.include_router(interactions.router, prefix="/api", tags=["Interactions"])
