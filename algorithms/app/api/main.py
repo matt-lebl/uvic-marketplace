54e085a948306ba9fe38fb1f3936b6dec15c0cdd
Matt Lebl <mlebl@uvic.ca>
2024-07-01 15:54:22 -0700

    Modify ruff source folder

--- algorithms/app/api/main.py
+++ algorithms/app/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.endpoints import search, recommendations, listings, interactions
 
-app = FastAPI() # this comment is only to introduce a new change to be detected by GitHub
+app = FastAPI() # this comment is only to introduce a new change to be detected by GitHub -
 
 app.include_router(listings.router, prefix="/api", tags=["Listings"])
 app.include_router(search.router, prefix="/api", tags=["Search"])
