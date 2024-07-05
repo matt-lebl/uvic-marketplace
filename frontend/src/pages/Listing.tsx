637ee8fd51c734920725f9ad9ce08b72ea4f3148
Ethan McCullagh <84647308+ethan89762@users.noreply.github.com>
2024-07-05 19:19:41 +0100

    added API get request

--- frontend/src/pages/Listing.tsx
+++ frontend/src/pages/Listing.tsx
@@ -4,10 +4,48 @@ import { Typography, Box, Paper } from '@mui/material'
 import PhotoGallery from './Components/PhotoGallery'
 import SellerCard from './Components/SellerCard'
 import { useParams } from 'react-router-dom'
+import { APIGet } from '../APIlink'
+import { ListingEntity } from '../interfaces'
+import {Button} from '@mui/material'
+
+function listingRequest(listingID : string | undefined) {
+
+  let response: ListingEntity | undefined
+
+  const listingURL: string = '/api/listing/' + listingID
+
+  const apiClick = () => {
+    setTimeout(async () => {
+      try {
+        response = await APIGet(listingURL)
+
+        if (response) {
+          console.log(response.title)
+          console.log(response)
+        }
+      } catch (error) {
+        console.log('Request Error' + error)
+      }
+
+    }, 1000)
+  }
+
+  return response
+
+}
 
 function Listing() {
 
-  let {listingID} = useParams();
+  const { listingID } = useParams();
+
+  const listingData = listingRequest(listingID);
+
+  if(listingData == undefined){
+    return <Typography>No listing found</Typography>
+  }
+
+
+
 
   return (
     <div className="Listing">
@@ -18,7 +56,7 @@ function Listing() {
             height: '85vh',
             backgroundColor: '#ffffff'
           }}>
-            <Typography sx={{fontWeight:'700', }}>Photo Gallery{listingID}</Typography>
+            <Typography sx={{ fontWeight: '700', }}>Photo Gallery{listingID}</Typography>
             <PhotoGallery />
           </Paper>
           <Paper
@@ -31,7 +69,7 @@ function Listing() {
               overflow: 'auto',
             }}
           >
-            <SellerCard />
+            <SellerCard data={listingData}/>
           </Paper>
         </Box>
       </header>
