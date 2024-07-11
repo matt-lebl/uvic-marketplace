1d25f87d59fbeb20f44f96263ffc105129dec76c
Levi <47727379+lbare@users.noreply.github.com>
2024-07-11 15:15:11 -0700

    Restrict page access when user isn't logged in

--- frontend/src/pages/Components/LoginHeader.tsx
+++ frontend/src/pages/Components/LoginHeader.tsx
@@ -4,27 +4,52 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
+import { useNavigate } from 'react-router-dom'
 
 export default function LoginHeader() {
+  const navigate = useNavigate()
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+      <AppBar
+        position="static"
+        sx={{
+          flexGrow: 1,
+          height: '8vh',
+          justifyContent: 'center',
+        }}
+      >
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            variant="h1"
+            component="div"
+            sx={{ flexGrow: 1, paddingX: 5 }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             UVic Marketplace
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Box
+            sx={{
+              width: '10vw',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Button
+              variant="text"
+              color="inherit"
+              onClick={() => navigate('/login')}
+            >
+              Login
+            </Button>
+            <Button
+              variant="text"
+              color="inherit"
+              onClick={() => navigate('/register')}
+            >
+              Register
+            </Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
