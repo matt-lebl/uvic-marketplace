1293528269164debb2d5ea4ca9c3c94b55afe1c2
KyleSullivanGitHub <75002173+KyleSullivanGitHub@users.noreply.github.com>
2024-07-12 20:03:08 -0700

    Update Search.tsx

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -13,19 +13,19 @@ function Search() {
 
   const searchFunc = async (searchRequest: SearchRequest): Promise<{ totalItems: number, items: ListingSummary[] }> => {
     // Add your logic here
-    // var results = {
-    //   totalItems: 0,
-    //   items: [] as ListingSummary[]
-    // }
-    // const queryParams: [string, string | number][] = Object.entries(searchRequest).filter(([key, value]) => value !== undefined && value !== null) as [string, string | number][];
-    // try {
-    //    const res = await APIGet<SearchResultsResponse>('/api/search', queryParams)
-    //    results.totalItems = res.totalItems
-    //    results.items = res.items
-    // } catch (e) {
-    //   console.error(e)
-    // }
-    // finally { return results }
+    var results = {
+       totalItems: 0,
+       items: [] as ListingSummary[]
+     }
+     const queryParams: [string, string | number][] = Object.entries(searchRequest).filter(([key, value]) => value !== undefined && value !== null) as [string, string | number][];
+     try {
+        const res = await APIGet<SearchResultsResponse>('/api/search', queryParams)
+        results.totalItems = res.totalItems
+        results.items = res.items
+     } catch (e) {
+       console.error(e)
+     }
+     finally { return results }
 
     // Mock Data
     const mockListings: ListingSummary[] = [
