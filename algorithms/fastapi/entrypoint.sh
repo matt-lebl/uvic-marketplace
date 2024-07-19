2a1da3e29b4c4b098877bdcff013d9c416fd8abf
Ross Goldsand <ross.goldsand@gmail.com>
2024-07-19 01:10:51 +0000

    move alembic directory and create new migration

--- algorithms/fastapi/entrypoint.sh
+++ algorithms/fastapi/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-alembic -c ./alembic.ini upgrade head && uvicorn main:app --host 0.0.0.0 --port 80
+alembic -c ./db/alembic.ini upgrade head && uvicorn main:app --host 0.0.0.0 --port 80
