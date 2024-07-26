071088b6cf8dc98b42b9a458aaea9406b414cabf
Ross Goldsand <ross.goldsand@gmail.com>
2024-07-26 05:52:05 +0000

    add extra error handling to algorithm containers

--- algorithms/fastapi/main.py
+++ algorithms/fastapi/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from endpoints import search, recommendations, listings, interactions, users
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_exception_handling(request: Request, call_next):
+    try:
+        response = await call_next(request)
+    except Exception as e:
+        print(f"Unhandled algorithms error: {e}")
+        return JSONResponse(status_code=500, content={"message": "Internal Server Error"})
+    return response
 
 app.include_router(listings.router, prefix="/api", tags=["Listings"])
 app.include_router(users.router, prefix="/api", tags=["Users"])
