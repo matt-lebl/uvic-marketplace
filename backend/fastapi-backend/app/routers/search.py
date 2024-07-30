1ab5fccadb5089530fbebbaab27e46ddbcf805a9
rahulniraula2 <rahulniraula47@gmail.com>
2024-07-30 05:53:52 -0700

    search error handle

--- backend/fastapi-backend/app/routers/search.py
+++ backend/fastapi-backend/app/routers/search.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from core.schemas import SearchTerm
 from services.data_layer_connect import send_request_to_data_layer
 
@@ -10,23 +10,34 @@ searchRouter = APIRouter(
 
 
 @searchRouter.post("/")
-async def add_search_history(searchTerm: SearchTerm, authUserID: str):
+async def add_search_history(
+    searchTerm: SearchTerm, authUserID: str, returnResponse: Response
+):
     path = "search-history/" + authUserID
+
+    if searchTerm.searchTerm.strip() == "":
+        returnResponse.status_code = 400
+        return {"error": "Search term cannot be empty"}
+
     response = await send_request_to_data_layer(
         path, "POST", data=searchTerm.model_dump()
     )
+
+    returnResponse.status_code = response.status_code
     return response.json()
 
 
 @searchRouter.get("")
-async def get_search_history(authUserID: str):
+async def get_search_history(authUserID: str, returnResponse: Response):
     path = "search-history/" + authUserID
     response = await send_request_to_data_layer(path, "GET")
+    returnResponse.status_code = response.status_code
     return response.json()
 
 
 @searchRouter.delete("")
-async def delete_search_history(authUserID: str):
+async def delete_search_history(authUserID: str, returnResponse: Response):
     path = "search-history/" + authUserID
     response = await send_request_to_data_layer(path, "DELETE")
+    returnResponse.status_code = response.status_code
     return response.json()
