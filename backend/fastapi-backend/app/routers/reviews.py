ece61e9ba62172421a16f5a6a268b45a8f6e5dde
rahulniraula2 <rahulniraula47@gmail.com>
2024-07-30 05:49:56 -0700

    reviews + messages error handle

--- backend/fastapi-backend/app/routers/reviews.py
+++ backend/fastapi-backend/app/routers/reviews.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from services.data_layer_connect import send_request_to_data_layer
 from core.schemas import NewReview
 
@@ -10,21 +10,26 @@ reviewsRouter = APIRouter(
 
 
 @reviewsRouter.post("")
-async def add_review(review: NewReview, authUserID: str):
+async def add_review(review: NewReview, authUserID: str, returnResponse: Response):
     path = "review/" + authUserID
     response = await send_request_to_data_layer(path, "POST", review.model_dump())
+    returnResponse.status_code = response.status_code
     return response.json()
 
 
 @reviewsRouter.patch("/{id}")
-async def edit_review(id: str, review: NewReview, authUserID: str):
+async def edit_review(
+    id: str, review: NewReview, authUserID: str, returnResponse: Response
+):
     path = "review/" + id + "/" + authUserID
     response = await send_request_to_data_layer(path, "PATCH", review.model_dump())
+    returnResponse.status_code = response.status_code
     return response.json()
 
 
 @reviewsRouter.delete("/{id}")
-async def delete_review(id: str, authUserID: str):
+async def delete_review(id: str, authUserID: str, returnResponse: Response):
     path = "review/" + id + "/" + authUserID
     response = await send_request_to_data_layer(path, "DELETE")
+    returnResponse.status_code = response.status_code
     return response.json()
