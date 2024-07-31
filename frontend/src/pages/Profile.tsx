6b0088763d0ba9f52bd45ff304a8b9908345a338
Levi <47727379+lbare@users.noreply.github.com>
2024-07-30 17:42:49 -0700

    Profile makes API calls with new endpoint

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -4,8 +4,6 @@ import {
   Box,
   Typography,
   Avatar,
-  Tabs,
-  Tab,
   TextField,
   Button,
   Pagination,
@@ -15,107 +13,30 @@ import {
   Grid,
 } from '@mui/material'
 import { User, ListingSummary } from '../interfaces'
-import { APIGet } from '../APIlink'
+import { APIGet, APIPost } from '../APIlink'
 
 const currentUser: User = {
-  userID: localStorage.getItem('userID') || '1',
-  username: localStorage.getItem('username') || 'firstlast',
-  name: localStorage.getItem('name') || 'First Last',
-  bio: localStorage.getItem('bio') || 'User Bio Here',
-  profileUrl:
-    localStorage.getItem('profileUrl') || 'https://randomuser.me/api/',
-  email: localStorage.getItem('email') || 'test@gmail.com',
+  userID: localStorage.getItem('userID') || '',
+  username: localStorage.getItem('username') || '',
+  name: localStorage.getItem('name') || '',
+  bio: localStorage.getItem('bio') || '',
+  profileUrl: localStorage.getItem('profileUrl') || '',
+  email: localStorage.getItem('email') || '',
 }
-
-const mockListings: ListingSummary[] = [
-  {
-    listingID: '1',
-    sellerID: '1',
-    sellerName: 'First Last',
-    title: 'Couch Item Title 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    price: 75,
-    dateCreated: '2023-01-01',
-    imageUrl:
-      'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y291Y2h8ZW58MHx8MHx8fDA%3D',
-    charityID: '1',
-  },
-  {
-    listingID: '2',
-    sellerID: '1',
-    sellerName: 'First Last',
-    title: 'Couch Item Title 2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    price: 50,
-    dateCreated: '2023-01-02',
-    imageUrl:
-      'https://images.unsplash.com/photo-1611967164521-abae8fba4668?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvdWNofGVufDB8fDB8fHww',
-    charityID: '1',
-  },
-  {
-    listingID: '3',
-    sellerID: '1',
-    sellerName: 'First Last',
-    title: 'Couch Item Title 3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    price: 75,
-    dateCreated: '2023-01-01',
-    imageUrl:
-      'https://images.unsplash.com/photo-1511401139252-f158d3209c17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvdWNofGVufDB8fDB8fHww',
-    charityID: '1',
-  },
-  {
-    listingID: '4',
-    sellerID: '1',
-    sellerName: 'First Last',
-    title: 'Couch Item Title 4',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    price: 50,
-    dateCreated: '2023-01-02',
-    imageUrl:
-      'https://images.unsplash.com/photo-1567016432779-094069958ea5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvdWNofGVufDB8fDB8fHww',
-    charityID: '1',
-  },
-  {
-    listingID: '5',
-    sellerID: '1',
-    sellerName: 'First Last',
-    title: 'Couch Item Title 5',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    price: 75,
-    dateCreated: '2023-01-01',
-    imageUrl:
-      'https://images.unsplash.com/photo-1590251024078-8a6d9f90b02d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGNvdWNofGVufDB8fDB8fHww',
-    charityID: '1',
-  },
-  {
-    listingID: '6',
-    sellerID: '1',
-    sellerName: 'First Last',
-    title: 'Couch Item Title 6',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    price: 50,
-    dateCreated: '2023-01-02',
-    imageUrl:
-      'https://images.unsplash.com/photo-1578500494198-246f612d3b3d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGNvdWNofGVufDB8fDB8fHww',
-    charityID: '1',
-  },
-]
 
 interface ProfileProps {
   user: User
-  listings: ListingSummary[]
 }
 
-const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
+const Profile: React.FC<ProfileProps> = ({ user }) => {
   const [editMode, setEditMode] = useState(false)
-  const [activeTab, setActiveTab] = useState(0)
   const [currentPage, setCurrentPage] = useState(1)
   const [listingsPerPage, setListingsPerPage] = useState(2)
-
-  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-    setActiveTab(newValue)
-  }
+  const [listings, setListings] = useState<ListingSummary[]>([])
+  const [loading, setLoading] = useState(true)
+  const [username, setUsername] = useState(user.username)
+  const [name, setName] = useState(user.name)
+  const [bio, setBio] = useState(user.bio)
 
   const handlePageChange = (
     event: React.ChangeEvent<unknown>,
@@ -128,10 +49,45 @@ const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
     setEditMode(!editMode)
   }
 
-  const currentListings = listings.slice(
-    (currentPage - 1) * listingsPerPage,
-    currentPage * listingsPerPage
-  )
+  const handleLogout = async () => {
+    try {
+      await APIPost('/api/user/logout')
+      localStorage.clear()
+      window.location.href = '/'
+    } catch (error) {
+      console.error('Failed to logout:', error)
+      alert('Logout failed. Please try again.')
+    }
+  }
+
+  const handleSave = async () => {
+    const updatedUser = {
+      username,
+      name,
+      email: user.email,
+      bio,
+      profilePictureUrl: user.profileUrl,
+      ignoreCharityListings: false,
+    }
+
+    try {
+      await APIPost('/api/user/', updatedUser)
+      localStorage.setItem('username', username)
+      localStorage.setItem('name', name)
+      localStorage.setItem('email', user.email)
+      localStorage.setItem('bio', bio)
+      setEditMode(false)
+    } catch (error) {
+      console.error('Failed to update profile:', error)
+    }
+  }
+
+  const handleCancel = () => {
+    setUsername(user.username)
+    setName(user.name)
+    setBio(user.bio)
+    setEditMode(false)
+  }
 
   const updateListingsPerPage = () => {
     const width = window.innerWidth
@@ -144,23 +100,33 @@ const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
     }
   }
 
-  const handleLogout = async () => {
-    try {
-      const logoutResponse = await APIGet<boolean>(`/api/user/logout`)
-    } catch (error) {
-      console.log(error)
-    } finally {
-      alert('Logged out successfully. See you later!')
-      localStorage.clear()
-      window.location.href = '/'
-    }
-  }
-
   useEffect(() => {
     updateListingsPerPage()
     window.addEventListener('resize', updateListingsPerPage)
     return () => window.removeEventListener('resize', updateListingsPerPage)
   }, [])
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const fetchedListings = await APIGet<ListingSummary[]>('/api/listing')
+        console.log(fetchedListings)
+
+        setListings(fetchedListings)
+      } catch (error) {
+        console.error('Failed to fetch listings:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchListings()
+  }, [user.userID])
+
+  const currentListings = listings.slice(
+    (currentPage - 1) * listingsPerPage,
+    currentPage * listingsPerPage
+  )
 
   return (
     <Box sx={{ margin: 6 }}>
@@ -182,23 +148,27 @@ const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
           {editMode ? (
             <Box>
               <TextField
-                label="Name"
-                defaultValue={user.name}
+                label="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 fullWidth
                 sx={{ marginBottom: 1 }}
               />
               <TextField
-                label="Email"
-                defaultValue={user.email}
+                label="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 fullWidth
                 sx={{ marginBottom: 1 }}
               />
               <TextField
                 label="Bio"
-                defaultValue={user.bio}
+                value={bio}
+                onChange={(e) => setBio(e.target.value)}
                 fullWidth
                 multiline
                 rows={4}
+                sx={{ marginBottom: 1 }}
               />
             </Box>
           ) : (
@@ -211,7 +181,7 @@ const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
             >
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <Typography variant="h6" fontSize="4rem">
-                  {user.name}
+                  {name}
                 </Typography>
               </Box>
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
@@ -223,34 +193,51 @@ const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
                 sx={{ display: 'flex', alignItems: 'center', paddingTop: 2 }}
               >
                 <Typography variant="h4" fontSize="1rem">
-                  {user.bio}
+                  {bio || ''}
                 </Typography>
               </Box>
             </Box>
           )}
         </Box>
         <Box sx={{ display: 'flex', alignSelf: 'stretch', gap: '10px' }}>
-          <Button
-            onClick={toggleEditMode}
-            sx={{ alignSelf: 'flex-start' }}
-            variant="contained"
-          >
-            {editMode ? 'Save' : 'Edit'}
-          </Button>
-          <Button
-            onClick={handleLogout}
-            sx={{ alignSelf: 'flex-start' }}
-            variant="contained"
-          >
-            Logout
-          </Button>
+          {editMode ? (
+            <>
+              <Button
+                onClick={handleSave}
+                sx={{ alignSelf: 'flex-start' }}
+                variant="contained"
+              >
+                Save
+              </Button>
+              <Button
+                onClick={handleCancel}
+                sx={{ alignSelf: 'flex-start' }}
+                variant="contained"
+                color="secondary"
+              >
+                Cancel
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={toggleEditMode}
+                sx={{ alignSelf: 'flex-start' }}
+                variant="contained"
+              >
+                Edit
+              </Button>
+              <Button
+                onClick={handleLogout}
+                sx={{ alignSelf: 'flex-start' }}
+                variant="contained"
+              >
+                Logout
+              </Button>
+            </>
+          )}
         </Box>
       </Box>
-      <Tabs value={activeTab} onChange={handleTabChange}>
-        <Tab label="Active Listings" />
-        <Tab label="Sold" />
-        <Tab label="Saved" />
-      </Tabs>
       <Box
         sx={{
           display: 'flex',
@@ -261,59 +248,87 @@ const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
         }}
       >
         <Grid container spacing={2} justifyContent="center">
-          {currentListings.map((listing) => (
-            <Grid item key={listing.listingID}>
-              <Card
-                key={listing.listingID}
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  marginBottom: 2,
-                  width: '40vh',
-                  height: '30vh',
-                  padding: '16px',
-                  borderRadius: '16px',
-                  boxSizing: 'border-box',
-                  background: '#B5DBFF',
-                }}
-              >
-                <Box
+          {loading ? (
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                minHeight: '20vh',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flex: 1,
+              }}
+            >
+              <Typography variant="h6">Loading...</Typography>
+            </Grid>
+          ) : listings.length === 0 ? (
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                minHeight: '20vh',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flex: 1,
+              }}
+            >
+              <Typography variant="h6">No Listings</Typography>
+            </Grid>
+          ) : (
+            currentListings.map((listing) => (
+              <Grid item key={listing.listingID}>
+                <Card
+                  key={listing.listingID}
                   sx={{
-                    width: '100%',
-                    height: '60%',
-                    overflow: 'hidden',
                     display: 'flex',
+                    flexDirection: 'column',
                     alignItems: 'center',
-                    justifyContent: 'center',
+                    marginBottom: 2,
+                    width: '40vh',
+                    height: '30vh',
+                    padding: '16px',
+                    borderRadius: '16px',
+                    boxSizing: 'border-box',
+                    background: '#B5DBFF',
                   }}
                 >
-                  <CardMedia
-                    component="img"
+                  <Box
                     sx={{
-                      height: '100%',
-                      width: 'auto',
-                      objectFit: 'fill',
-                      borderRadius: '10px',
+                      width: '100%',
+                      height: '60%',
+                      overflow: 'hidden',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
                     }}
-                    image={listing.imageUrl}
-                    alt={listing.title}
-                  />
-                </Box>
-                <CardContent sx={{ width: '100%' }}>
-                  <Typography component="h5" variant="h5">
-                    {listing.title}
-                  </Typography>
-                  <Typography variant="subtitle1" color="textSecondary">
-                    ${listing.price}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {listing.description}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        height: '100%',
+                        width: 'auto',
+                        objectFit: 'fill',
+                        borderRadius: '10px',
+                      }}
+                      image={listing.imageUrl}
+                      alt={listing.title}
+                    />
+                  </Box>
+                  <CardContent sx={{ width: '100%' }}>
+                    <Typography component="h5" variant="h5">
+                      {listing.title}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                      ${listing.price}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {listing.description}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))
+          )}
         </Grid>
       </Box>
       <Box
@@ -323,18 +338,19 @@ const Profile: React.FC<ProfileProps> = ({ user, listings }) => {
           marginTop: 2,
         }}
       >
-        <Pagination
-          count={Math.ceil(listings.length / listingsPerPage)}
-          page={currentPage}
-          onChange={handlePageChange}
-        />
+        {listings.length > listingsPerPage && !loading && (
+          <Pagination
+            count={Math.ceil(listings.length / listingsPerPage)}
+            page={currentPage}
+            onChange={handlePageChange}
+          />
+        )}
       </Box>
     </Box>
   )
 }
-
 const ProfileContainer: React.FC = () => {
-  return <Profile user={currentUser} listings={mockListings} />
+  return <Profile user={currentUser} />
 }
 
 export default ProfileContainer
