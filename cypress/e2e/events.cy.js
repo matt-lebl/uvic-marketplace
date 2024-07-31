a11a5181ca3d7cb5cbdf1a74e3ad66eea2897480
Ivan Denyssevych <ivan.denyssevych7@gmail.com>
2024-07-31 00:11:45 -0700

    adding charity events page load test

--- cypress/e2e/events.cy.js
+++ cypress/e2e/events.cy.js
@@ -1,7 +1,24 @@
+const email = Cypress.env('e2e_email');
+const password = Cypress.env('e2e_password');
+const totp_secret = Cypress.env('totp_secret');
+
 describe('Events', () => {
+  beforeEach(() => {
+    cy.visit("https://market.lebl.ca/login");
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+    cy.generateTotp(totp_secret).then((totp_code) => {
+      cy.get('input[name="totp_code"]').type(totp_code);
+    })
+    cy.get('form[data-testid="login-form"]').submit();
+    cy.url().should("eq", "https://market.lebl.ca/");
+    cy.get("button").eq(3).click();
+  });
+
   //TODO: implement
   it('Should display events', () => {
-    cy.visit('https://market.lebl.ca/login')
+    cy.url().should("eq", "https://market.lebl.ca/events");
+    cy.contains("h4", "Charity Events").should("be.visible");
   })
 
   it('Should view an event', () => {
