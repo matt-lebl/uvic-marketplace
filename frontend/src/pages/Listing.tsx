fd9b1114f0fb5e6d861f952e754aecfbfc829885
Levi <47727379+lbare@users.noreply.github.com>
2024-07-31 09:25:54 -0700

    Add edit functionality for a user's own listing

--- frontend/src/pages/Listing.tsx
+++ frontend/src/pages/Listing.tsx
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import { Typography, Box, Paper } from '@mui/material'
+import { Typography, Box, Paper, Button } from '@mui/material'
 import PhotoGallery from './Components/PhotoGallery'
 import SellerCard from './Components/SellerCard'
-import { useParams } from 'react-router-dom'
 import { APIGet } from '../APIlink'
 import { ListingEntity } from '../interfaces'
+import { useParams, useNavigate } from 'react-router-dom'
 import Reviews from './Components/Reviews'
-
 
 interface ListingProps {
   listingData?: ListingEntity
 }
 
-const Listing: React.FC<ListingProps> = ({ listingData: initialListingData, }) => {
+const Listing: React.FC<ListingProps> = ({
+  listingData: initialListingData,
+}) => {
   const { listingID } = useParams<string>()
   const [listingData, setListingData] = useState<ListingEntity | undefined>(
     initialListingData
   )
   const [loading, setLoading] = useState(!initialListingData)
+  const [userListing, setUserListing] = useState<boolean>(false)
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     async function fetchListing() {
       if (!listingID || initialListingData) return
-
       const listingURL: string = `/api/listing/${listingID}`
 
       try {
         const response = (await APIGet(listingURL)) as ListingEntity
+
         if (response) {
           setListingData(response)
+          checkUserListing(response)
         }
       } catch (error) {
         console.log('Request Error', error)
@@ -37,8 +42,19 @@ const Listing: React.FC<ListingProps> = ({ listingData: initialListingData, }) =
         setLoading(false)
       }
     }
+
     fetchListing()
   }, [listingID, initialListingData])
+
+  const checkUserListing = (listing: ListingEntity) => {
+    const userID = localStorage.getItem('userID')
+
+    if (userID === listing.seller_profile.userID) {
+      setUserListing(true)
+    } else {
+      setUserListing(false)
+    }
+  }
 
   if (loading) {
     return <div>Loading...</div>
@@ -51,14 +67,16 @@ const Listing: React.FC<ListingProps> = ({ listingData: initialListingData, }) =
   return (
     <div className="Listing">
       <header className="App-header">
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          alignItems: 'center',
-          marginTop: 1,
-          width: '90%',
-        }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            alignItems: 'center',
+            marginTop: 1,
+            width: '90%',
+          }}
+        >
           <Box sx={{ display: 'flex', flexDirection: 'row' }}>
             <Paper
               sx={{
@@ -75,13 +93,34 @@ const Listing: React.FC<ListingProps> = ({ listingData: initialListingData, }) =
                 minWidth: '40vw',
                 ml: 5,
                 backgroundColor: '#656565',
-                height: '85vh',
-                overflow: 'auto',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                padding: '20px',
+              }}
+              component="form"
+              onSubmit={(e) => {
+                e.preventDefault()
+
+                if (userListing) {
+                  navigate(`/edit-listing/${listingID}`)
+                }
               }}
             >
-              <SellerCard data={listingData} />
-              <Reviews listingID={listingID} initialReviews={listingData?.reviews ?? []} />
-
+              <Box>
+                <SellerCard data={listingData} />
+                <Reviews
+                  listingID={listingID}
+                  initialReviews={listingData?.reviews ?? []}
+                />
+              </Box>
+              {userListing && (
+                <Box display={'flex'} flexDirection={'row-reverse'}>
+                  <Button variant="contained" type="submit">
+                    Edit
+                  </Button>
+                </Box>
+              )}
             </Paper>
           </Box>
         </Box>
